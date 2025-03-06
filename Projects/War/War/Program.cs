dbe62a39ce8387e9c14599d61d0b323298dcbd99
april536 <song.zhuor@northeastern.edu>
2025-03-06 14:43:46 -0500

    Update Program.cs

--- Projects/War/War/Program.cs
+++ Projects/War/War/Program.cs
@@ -9,9 +9,15 @@ Card playerHand = new();//player’s card
 Card dealerHand = new();//dealer’s card
 int playerScore = 0;//player’s score
 int dealerScore = 0;//dealer’s score
-
+bool closeRequested = false;
+State state = State.MainMenu;
+Player player = new Player();
+Player dealer = new Player();
 
 try {
+    while (!closeRequested) {
+        Gameloop();
+    }
     // Add 52 cards with 4 different suits combined with 13 values
     foreach (Suit suit in Enum.GetValues<Suit>()) {
         foreach (Value value in Enum.GetValues<Value>()) {
@@ -74,7 +80,21 @@ try {
         if (Console.ReadKey(true).Key == ConsoleKey.Escape) return;
     }
 } finally {
-    Console.WriteLine("Game End");
+    Console.WriteLine("Esc pressed. Game closed.");
+}
+
+void Gameloop() {
+    switch (state) {
+        case State.MainMenu:
+            ShowMainMenu();
+            break;
+        case State.ShowCardSkills:
+            ShowCardSkills();
+            break;
+        case State.RandomizeTurn:
+            RandomizeTurn();
+            break;
+    }
 }
 
 static void Shuffle(List<Card> cards) { // Method to shuffle the cards
@@ -85,12 +105,70 @@ static void Shuffle(List<Card> cards) { // Method to shuffle the cards
     }
 }
 
+void ShowMainMenu() {
+    Console.Clear();
+    Console.WriteLine("War Game");
+    Console.WriteLine("This is a doule-player stratigic card game!");
+    Console.WriteLine("Both players draw 2 cards at start of the turn.");
+    Console.WriteLine("The first card's value shall be count into the value of this turn.");
+    Console.WriteLine("The second card can be used either as value card or skill card.");
+    Console.WriteLine("   [1. Value card would be added to the value of this turn.]");
+    Console.WriteLine("   [2. Skill card would trigger the skill related to it's suit.]");
+    Console.WriteLine("Player who holds the higher value wins the turn and earn score.");
+    Console.WriteLine("Game ends when one player's score reaches 10!");
+    Console.WriteLine("    1. Press 1 to stard PVP Mode");
+    Console.WriteLine("    2. Press 2 to start PVE Mode");
+    Console.WriteLine("    3. Press 3 to check the skills of cards");
+    Console.WriteLine("    Press Esc to exit");
+    switch (Console.ReadKey(true).Key) {
+        case ConsoleKey.D1:
+            state = State.RandomizeTurn;
+            break;
+        case ConsoleKey.D2:
+            state = State.RandomizeTurn;
+            break;
+        case ConsoleKey.D3:
+            state = State.RandomizeTurn;
+            break;
+        case ConsoleKey.Escape:
+            closeRequested = true;
+            break;
+    }
+}
+
+void ShowCardSkills() {
+    Console.Clear();
+    Console.WriteLine("Card Skills");
+    Console.WriteLine("Wait for the next update!");
+    Console.WriteLine("Press Enter to return to main menu");
+    if (Console.ReadKey(true).Key == ConsoleKey.Enter) {
+        state = State.MainMenu;
+    }
+    if (Console.ReadKey(true).Key == ConsoleKey.Escape) closeRequested = true;
+}
+
+void RandomizeTurn() {
+    Console.Clear();
+    Console.WriteLine("Randomizing turn...");
+    Console.WriteLine("Press Enter to continue");
+    if (Console.ReadKey(true).Key == ConsoleKey.Enter) {
+        state = State.DealCards;
+    }
+    if (Console.ReadKey(true).Key == ConsoleKey.Escape) closeRequested = true;
+}
+
+class Player {
+    public bool Gofirst { get; set; }
+    public bool isHuman { get; set; }
+    public int Score { get; set; }
+    public List<Card> Hand { get; set; } = new List<Card>();
+}
+
 class Card
 {
     // A class of cards with two kinds of properties.
     public Suit Suit { get; set; }
     public Value Value { get; set; }
-    public const int RenderHeight = 7;
 
     public string Render()
     {
@@ -147,4 +225,22 @@ enum Value
     Jack = 11,
     Queen = 12,
     King = 13
+}
+
+enum State {
+    // The game’s state.
+    MainMenu,
+    ShowCardSkills,
+    RandomizeTurn,
+    DealCards,
+    HumanTurn,
+    CPUTurn,
+    ScoreCounter,
+    ShowWinner,
+}
+
+enum GameMode {
+    // The game’s mode.
+    PVP,
+    PVE
 }
